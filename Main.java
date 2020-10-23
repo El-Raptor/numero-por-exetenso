5ae816de671f897e571752c1a28ac2f3ed371ecc
Felipe Salles Lopes <felipe28101997@gmail.com>
2020-10-22 21:35:21 -0400

    Adicionado suporte a milhares

--- Main.java
+++ Main.java
@@ -4,7 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		Reading r = new Reading();
 		
-		for (int i = 0; i < 1000; i++)
-			System.out.println(r.read(i).toUpperCase());
+		int i = 1024;
+		
+		while (i < 10000) {
+			System.out.println(r.read(i).toUpperCase() + " " + i);
+			i = i + 500;
+		}
 	}
 }
--- Reading.java
+++ Reading.java
@@ -1,230 +1,233 @@
 package Extenso;
 
 public class Reading {
-	
+
 	public Reading() {
-		
+
 	}
-	
+
 	private int digitsCount(int number) {
 		String n = String.valueOf(number);
-		
+
 		return n.length();
 	}
-	
+
 	public String read(int number) {
 		int digits = digitsCount(number);
-		
+
 		if (digits == 1)
 			return unity(number);
 		if (digits == 2)
 			return twoDigits(number);
-		
-		return hundreds(number);
+		if (digits == 3)
+			return hundreds(number);
+
+		return thousands(number);
 	}
-	
+
 	private String unity(int number) {
-		
+
 		if (number == 0)
 			return "Zero";
-		
+
 		if (number == 1)
 			return "Um";
-		
+
 		if (number == 2)
 			return "Dois";
-		
+
 		if (number == 3)
 			return "Três";
-		
+
 		if (number == 4)
 			return "Quatro";
-		
+
 		if (number == 5)
 			return "Cinco";
-		
+
 		if (number == 6)
 			return "Seis";
-		
+
 		if (number == 7)
 			return "Sete";
-		
+
 		if (number == 8)
 			return "Oito";
-		
+
 		return "Nove";
 	}
-	
+
 	private String twoDigits(int number) {
 		String dicker = "";
 		String n = String.valueOf(number);
-		
-		if (n.charAt(0) == '0')
-			return unity(number % 10);
-		
+
 		if (n.charAt(0) == '1') {
 			if (number == 10)
 				return "Dez";
-			
-			else if (number == 11)
+
+			if (number == 11)
 				return "Onze";
-			
-			else if (number == 12)
+
+			if (number == 12)
 				return "Doze";
-			
-			else if (number == 13)
+
+			if (number == 13)
 				return "Treze";
-			
-			else if (number == 14)
+
+			if (number == 14)
 				return "Quatorze";
-			
-			else if (number == 15)
+
+			if (number == 15)
 				return "Quinze";
-			
-			else if (number == 16)
+
+			if (number == 16)
 				return "Dezesseis";
-			
-			else if (number == 17)
+
+			if (number == 17)
 				return "Dezessete";
-			
-			else if (number == 18)
+
+			if (number == 18)
 				return "Dezoito";
-			
-			else
-				return "Dezenove";
+
+			return "Dezenove";
 		}
-		
+
 		if (n.charAt(0) == '2') {
 			if (number == 20)
 				return "Vinte";
-			
-			dicker = "Vinte";			
-			dicker += " E " + unity(number % 10);
+
+			dicker = "Vinte E " + unity(number % 10);
 		} else if (n.charAt(0) == '3') {
 			if (number == 30)
 				return "Trinta";
-			
-			dicker = "Trinta";			
-			dicker += " E " + unity(number % 10);
+
+			dicker = "Trinta E " + unity(number % 10);
 		} else if (n.charAt(0) == '4') {
 			if (number == 40)
 				return "Quarenta";
-			
-			dicker = "Quarenta";			
-			dicker += " E " + unity(number % 10);
+
+			dicker = "Quarenta E " + unity(number % 10);
 		} else if (n.charAt(0) == '5') {
 			if (number == 50)
 				return "Cinquenta";
-			
-			dicker = "Cinquenta";			
-			dicker += " E " + unity(number % 10);
+
+			dicker = "Cinquenta E " + unity(number % 10);
 		} else if (n.charAt(0) == '6') {
 			if (number == 60)
 				return "Sessenta";
-			
-			dicker = "Sessenta";			
-			dicker += " E " + unity(number % 10);
+
+			dicker = "Sessenta E " + unity(number % 10);
 		} else if (n.charAt(0) == '7') {
 			if (number == 70)
 				return "Setenta";
-			
-			dicker = "Setenta";			
-			dicker += " E " + unity(number % 10);
+
+			dicker = "Setenta E " + unity(number % 10);
 		} else if (n.charAt(0) == '8') {
 			if (number == 80)
 				return "Oitenta";
-			
-			dicker = "Oitenta";			
-			dicker += " E " + unity(number % 10);
+
+			dicker = "Oitenta E " + unity(number % 10);
 		} else if (n.charAt(0) == '9') {
 			if (number == 90)
 				return "Noventa";
-			
-			dicker = "Noventa";			
-			dicker += " E " + unity(number % 10);
+
+			dicker = "Noventa E " + unity(number % 10);
 		}
-		
+
 		return dicker;
 	}
-	
+
 	private String hundreds(int number) {
 		String hundred = "";
 		String n = String.valueOf(number);
-		
+		int digits = digitsCount(number % 100);
+
 		if (n.charAt(0) == '1') {
 			if (number == 100)
 				return "Cem";
 
-			int digits = digitsCount(number % 100);
 			hundred = "Cento E ";
 			// Verifica se o resto da divisão por 100 tem duas ou uma casa.
-			hundred += digits > 1 ?  twoDigits(number % 100) : unity(number % 100);
+			hundred += digits > 1 ? twoDigits(number % 100) : unity(number % 100);
 		} else if (n.charAt(0) == '2') {
 			hundred = "duzentos";
 			if (number % 200 == 0)
 				return hundred;
-			
-			int digits = digitsCount(number % 100);
+
 			hundred += " E ";
-			hundred += digits > 1 ?  twoDigits(number % 100) : unity(number % 100);
+			hundred += digits > 1 ? twoDigits(number % 100) : unity(number % 100);
 		} else if (n.charAt(0) == '3') {
 			hundred = "trezentos";
 			if (number % 300 == 0)
 				return hundred;
-			
-			int digits = digitsCount(number % 100);
+
 			hundred += " E ";
-			hundred += digits > 1 ?  twoDigits(number % 100) : unity(number % 100);
+			hundred += digits > 1 ? twoDigits(number % 100) : unity(number % 100);
 		} else if (n.charAt(0) == '4') {
 			hundred = "qatrocentos";
 			if (number % 400 == 0)
 				return hundred;
-			
-			int digits = digitsCount(number % 100);
+
 			hundred += " E ";
-			hundred += digits > 1 ?  twoDigits(number % 100) : unity(number % 100);
+			hundred += digits > 1 ? twoDigits(number % 100) : unity(number % 100);
 		} else if (n.charAt(0) == '5') {
 			hundred = "quinhentos";
 			if (number % 500 == 0)
 				return hundred;
-			
-			int digits = digitsCount(number % 100);
+
 			hundred += " E ";
-			hundred += digits > 1 ?  twoDigits(number % 100) : unity(number % 100);
+			hundred += digits > 1 ? twoDigits(number % 100) : unity(number % 100);
 		} else if (n.charAt(0) == '6') {
 			hundred = "seiscentos";
 			if (number % 600 == 0)
 				return hundred;
-			
-			int digits = digitsCount(number % 100);
+
 			hundred += " E ";
-			hundred += digits > 1 ?  twoDigits(number % 100) : unity(number % 100);
+			hundred += digits > 1 ? twoDigits(number % 100) : unity(number % 100);
 		} else if (n.charAt(0) == '7') {
 			hundred = "setecentos";
 			if (number % 700 == 0)
 				return hundred;
-			
-			int digits = digitsCount(number % 100);
+
 			hundred += " E ";
-			hundred += digits > 1 ?  twoDigits(number % 100) : unity(number % 100);
-		}else if (n.charAt(0) == '8') {
+			hundred += digits > 1 ? twoDigits(number % 100) : unity(number % 100);
+		} else if (n.charAt(0) == '8') {
 			hundred = "oitocentos";
 			if (number % 800 == 0)
 				return hundred;
-			
-			int digits = digitsCount(number % 100);
+
 			hundred += " E ";
-			hundred += digits > 1 ?  twoDigits(number % 100) : unity(number % 100);
+			hundred += digits > 1 ? twoDigits(number % 100) : unity(number % 100);
 		} else if (n.charAt(0) == '9') {
 			hundred = "novecentos";
 			if (number % 900 == 0)
 				return hundred;
-			
-			int digits = digitsCount(number % 100);
+
 			hundred += " E ";
-			hundred += digits > 1 ?  twoDigits(number % 100) : unity(number % 100);
+			hundred += digits > 1 ? twoDigits(number % 100) : unity(number % 100);
 		}
-			
+
 		return hundred;
+	}
+
+	private String thousands(int number) {
+		String thousands = "";
+		int digits = digitsCount(number % 1000);
+		int leadingNumber = number / 1000;
+
+		if (leadingNumber == 1)
+			thousands = "Mil";
+		else
+			thousands = unity(leadingNumber) + " Mil";
+
+		if (number % 1000 != 0) {
+
+			thousands += " E ";
+			thousands += digits == 3 ? hundreds(number % 1000)
+					: digits > 1 ? twoDigits(number % 1000) : unity(number % 1000);
+			return thousands;
+		}
+
+		return thousands;
 	}
 }
